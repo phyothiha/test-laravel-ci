ff54fd4d6e9336455686d048459af24a9141ba24
Phyo Thiha <phyothiha96@gmail.com>
2025-01-24 23:58:04 +0630

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable {
-    use HasApiTokens, 
-    HasFactory, 
-    Notifiable;
+class User extends Authenticatable
+{
+    // nothing out of ordinary
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
